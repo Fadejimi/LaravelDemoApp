0bba46fb7be4e775ddd5e6cbcb3b234e8022fd9d
Adegbulugbe Fadejimi <adegbulugbefadejimi@gmail.com>
2017-02-27 17:20:57 +0100

    Initial commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+  <link href="css/styles.css" rel="stylesheet" type="text/css">
+  @yield('header')
+</head>
+
+<body>
+
+    @yield('content')
+</body>
+
+@yield('footer')
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+   <b>The welcome page goes here</b>
+@stop
